71be4b5261b24e5660a5dd77285465f1f8e0814a
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2021-04-30 00:31:04 +0000

    Feat(user): Add isAuth utility to context

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,44 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, AuthenticationError } from "apollo-server";
 import { PrismaClient } from "@prisma/client";
 import { config } from "dotenv";
+import jwt from "jsonwebtoken";
 import typeDefs from "./type-defs";
 import resolvers from "./resolvers";
 import UserDataSource from "./datasource/user-datasource";
+import { App } from "./types/app";
 
 config();
 
 const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers,
-  dataSources: () => {
+
+  context({ req }) {
+    const authHeader = req.header("authorization");
+
+    return {
+      isAuth(): App.JWTPayload {
+        if (!authHeader) {
+          throw new AuthenticationError("unauthorized");
+        }
+
+        const [label, token] = authHeader.split(" ");
+
+        if (label != "Bearer") {
+          throw new AuthenticationError("unauthorized");
+        }
+
+        const jwtPayload = jwt.verify(
+          token,
+          process.env.JWT_SECRET_KEY as string
+        ) as App.JWTPayload;
+
+        return jwtPayload;
+      }
+    };
+  },
+
+  dataSources() {
     const prisma = new PrismaClient();
 
     return {
