2f9d66bd4613a4d0e4e90f0e76d74c55212c602f
ifarahi <dr0farahi@gmail.com>
2021-05-01 22:42:21 +0000

    Fix(accounts): extend the create account schema

--- app/joi-schemes/account-schemes.ts
+++ app/joi-schemes/account-schemes.ts
@@ -1,6 +1,9 @@
 import Joi from "joi";
 
 export const createAccountSchema = Joi.object({
-  bankName: Joi.string().min(3).max(200).required(),
-  accountNumber: Joi.string().min(10).max(200).required()
+  bankName: Joi.string().min(3).max(200).trim().required(),
+  accountNumber: Joi.string()
+    .regex(/[0-9]{10,200}/)
+    .trim()
+    .required()
 });
