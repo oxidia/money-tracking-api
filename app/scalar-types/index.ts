44e2c6f6407ebcdfc07de0ad29af39e3eff3c170
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2021-05-08 22:18:33 +0000

    Fix(types): Fix new scalar type(Date)
    - Make parseLiteral and parseValue to parse timestamp and string
    - throw an exception if JS Date failed to parse the given value

--- app/scalar-types/index.ts
+++ app/scalar-types/index.ts
@@ -1,4 +1,4 @@
-const { GraphQLScalarType } = require("graphql");
+import { GraphQLScalarType, Kind, ASTNode, GraphQLError } from "graphql";
 
 export const ScalarDate = {
   Date: new GraphQLScalarType({
@@ -6,19 +6,39 @@ export const ScalarDate = {
     description: "Date custom scalar type",
 
     serialize(value: Date): number {
-      return value.getTime();
-    },
+      const timestamp = value.getTime();
 
-    parseValue(value?: string | number): Date {
-      if (!value) {
-        return new Date();
-      } else {
-        return new Date(value);
+      if (isNaN(timestamp)) {
+        throw new GraphQLError("GRAPHQL_VALIDATION_FAILED");
       }
+
+      return timestamp;
     },
 
-    parseLiteral(ast: any): Date {
-      return new Date(ast.value);
+    parseValue(value: string | number): Date {
+      const date = new Date(value);
+
+      if (isNaN(date.getTime())) {
+        throw new GraphQLError("GRAPHQL_VALIDATION_FAILED");
+      }
+
+      return date;
+    },
+
+    parseLiteral(ast: ASTNode): Date {
+      var date = null;
+
+      if (ast.kind == Kind.INT) {
+        date = new Date(parseInt(ast.value, 10));
+      } else if (ast.kind == Kind.STRING) {
+        date = new Date(ast.value);
+      }
+
+      if (!date || isNaN(date.getTime())) {
+        throw new GraphQLError("GRAPHQL_VALIDATION_FAILED");
+      }
+
+      return date;
     }
   })
 };
