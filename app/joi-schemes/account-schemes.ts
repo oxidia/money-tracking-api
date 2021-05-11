5bd97872157ea54197f83f26e02325fc301c5df5
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2021-05-11 00:46:15 +0000

    Fix(account): Create account query validation schema

--- app/joi-schemes/account-schemes.ts
+++ app/joi-schemes/account-schemes.ts
@@ -1,5 +1,9 @@
 import Joi from "joi";
 
+export const accountSchema = Joi.object({
+  accountId: Joi.number().integer().min(1).required()
+});
+
 export const createAccountSchema = Joi.object({
   bankName: Joi.string().min(3).max(200).trim().required(),
   accountNumber: Joi.string()
